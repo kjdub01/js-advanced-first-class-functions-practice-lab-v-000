89d0a3a304ee81d3b345c6a3164412cd6b363ea3
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-07 23:25:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,6 @@ const driversByRevenue = function (drivers) {
 };
 
 const driverByName = function (drivers) {
-  
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
 };
